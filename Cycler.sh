3daa7fe0aa15b9a1831b041393511f2f9f1c47f6
Jacksaur <58850292+Jacksaur@users.noreply.github.com>
2022-12-05 09:11:15 +0000

    Zero pad, support numbers higher than 8
    You do not want to know how long this took.
    Bash is very old and filled with infuriating old tendancies.

--- Cycler.sh
+++ Cycler.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# !!TEMPORARY!! CHANGE ME!
-cd ./Cycler/
-# !!
+# Set working directory to script's location
+cd "${0%/*}"
 count=0
 
+#Background cycling
 # Iterate through files and decrease their filename by 1. Also track how many total backgrounds are available.
 for f in *.png; do
-	echo Renaming $f to $((${f%.*}-1)).png
-	mv $f $((${f%.*}-1)).png
+	echo Renaming $f to $(printf %02d $((10#${f%.*}-1)))
+	mv $f $(printf %02d $((10#${f%.*}-1))).png
 	count=$((++count))
 done
 
 # Remove background from main folder and rename it to sit at bottom of the rotation, put the lowest value image back in its place.
-echo Swapping background now...
-mv ../background.png ./$count.png
-mv ./0.png ../background.png
+echo Swapping background with $(printf %02d $(($count)))
+mv ../background.png $(printf %02d $(($count))).png
+mv ./00.png ../background.png
